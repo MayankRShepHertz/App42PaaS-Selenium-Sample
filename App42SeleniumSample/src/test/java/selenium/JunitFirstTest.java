853154ba3681031f9e4df7fc0879d34015cb3daf
ashutosh.yadav <ashutosh.yadav@shephertz.com>
2015-07-17 15:56:46 +0530

    change sample

--- App42SeleniumSample/src/test/java/selenium/JunitFirstTest.java
+++ App42SeleniumSample/src/test/java/selenium/JunitFirstTest.java
@@ -35,4 +35,76 @@ public class JunitFirstTest {
         Assert.assertEquals(driver.getTitle(), "Google");
         
     }
+	@Test
+    public void myTest2() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest3() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest4() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest5() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest6() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest7() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest8() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest9() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest10() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest11() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest12() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest132() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest142() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest152() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest162() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest172() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest182() {
+		Assert.assertEquals("Google", "Google");
+	}
+	@Test
+    public void myTest192() {
+		Assert.assertEquals("Google", "Google");
+	}
 }
